337fd8884475d6279488a1d52c2af38ab7a65867
ArchangelX360 <titouan.bion@gmail.com>
2022-01-27 17:07:43 +0100

    Upgrade examples to version `0.0.10`

--- examples/large-generated-project/build.gradle.kts
+++ examples/large-generated-project/build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 }
 
 plugins {
-    id("io.github.archangelx360.icon-generator") version "0.0.9"
+    id("io.github.archangelx360.icon-generator") version "0.0.10"
 }
 
 generateIconsForSources {
--- examples/small-project/build.gradle.kts
+++ examples/small-project/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("io.github.archangelx360.icon-generator") version "0.0.9"
+    id("io.github.archangelx360.icon-generator") version "0.0.10"
 }
 
 generateIconsForSources {
